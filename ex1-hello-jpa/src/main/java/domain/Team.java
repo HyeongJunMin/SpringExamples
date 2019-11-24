21cc7f6c534b06f20f1886070a16564cdd75e8b3
HyeongJunMin <alsgudwns007@naver.com>
2019-11-24 15:46:58 +0900

    + compare value

--- ex1-hello-jpa/src/main/java/domain/Team.java
+++ ex1-hello-jpa/src/main/java/domain/Team.java
@@ -5,6 +5,7 @@ import lombok.*;
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Getter @Setter @ToString
@@ -19,4 +20,19 @@ public class Team extends BaseEntity {
     //연관관계의 주인이 아니기 때문에 members의 값이 어떻게 바뀌든 DB에는 영향이 가지 않
     @OneToMany(mappedBy = "team")
     private List<Member> members = new ArrayList();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Team team = (Team) o;
+        return Objects.equals(id, team.id) &&
+                Objects.equals(name, team.name) &&
+                Objects.equals(members, team.members);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, members);
+    }
 }
