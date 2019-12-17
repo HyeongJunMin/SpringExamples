e7dc8ed29839b0fc8b11a3eac2304bda5b15c660
HyeongJunMin <alsgudwns007@naver.com>
2019-12-17 10:22:32 +0900

    springboot + mariadb 연습 프로젝트

---
+++ springbootJPAMaria/src/main/java/com/jpamaria/mariaexam/MariaexamApplication.java
@@ -0,0 +1,13 @@
+package com.jpamaria.mariaexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MariaexamApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MariaexamApplication.class, args);
+  }
+
+}
---
+++ springbootJPAMaria/src/test/java/com/jpamaria/mariaexam/MariaexamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpamaria.mariaexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MariaexamApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
